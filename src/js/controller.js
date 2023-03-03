91f22560d21b52252e17daa8792272cfbc23286e
Jitendra Kumar Gupta <jitendrakr54@gmail.com>
2023-03-03 17:34:34 +0530

    Clean up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,9 +132,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
+// const newFeature = function () {
+//   console.log('Welcome to the application!');
+// };
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookMarks);
@@ -144,6 +144,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
+  // newFeature();
 };
 init();
